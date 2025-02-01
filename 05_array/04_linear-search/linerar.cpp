762776d57fbb90d5586d64e14fe7d11f5fdf7d2b
ankitmeofficial <136215049+ankitmeofficial@users.noreply.github.com>
2025-02-01 09:27:30 +0530

    1 feb 2025

---
+++ 05_array/04_linear-search/linerar.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+bool search (int arr[], int size , int key ){
+
+    for( int i=0 ;i< size ; i++ ){
+        if (arr[i]==key){
+            return 1;
+        }
+        return 0;
+    }
+
+
+}
+
+int main()
+{
+   int arr[5]={1,2,3,4,5};
+   int key ;
+   cout<<"enter a element for search "<< endl;
+   cin >> key ;
+
+
+
+}
+
+
+
